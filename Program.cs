abab17c0366eba19d3de484905a9b8b2ba1bd1d1
olumideo <mide.oluyi@gmail.com>
2020-09-28 22:22:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1985;
+            myCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            // decimal value = DetermineMv(myCar);
+            // Console.WriteLine("{0:C}", value);
+            Console.WriteLine("{0:C}", myCar.DetermineMv());
+
+            Console.ReadLine();
+
+
+        }
+        private static decimal DetermineMv(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMv()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+            return carValue;
+        }
+
+    }
+}
